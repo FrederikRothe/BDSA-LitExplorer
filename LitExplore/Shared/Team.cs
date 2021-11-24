b63f5b9fd5be9f9ca7b11715c904fec05f9fa2d4
Sebastian Fugmann <sefu@itu.dk>
2021-11-24 13:25:59 +0100

    Changed the framework

--- LitExplore/Shared/Team.cs
+++ LitExplore/Shared/Team.cs
@@ -3,14 +3,18 @@ namespace LitExplore.Shared;
 public class Team
 {
     public int Id { get; set; }
+
+    public User TeamLeader { get; set; }
     
     public ICollection<User> Users { get; set; }
 
     public string TeamName { get; set; }
 
-    public Team(ICollection<User> users, string teamName)
+    public Team(User teamLeader, string teamName)
     {
-        Users = users;
+        TeamLeader = teamLeader;
+        Users = new List<User>();
+        Users.Add(teamLeader);
         TeamName = teamName;
     }
 }
