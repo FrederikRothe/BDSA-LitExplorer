45a1d71580a42634c4a69abad945bc367ec60666
SebastianVFugmann <90041204+SebastianVFugmann@users.noreply.github.com>
2021-11-24 13:48:20 +0100

    Team (#7)
    * Team framework
    
    * Changed the framework
    
    Co-authored-by: Sebastian Fugmann <sefu@itu.dk>

---
+++ LitExplore/Shared/Team.cs
@@ -0,0 +1,20 @@
+namespace LitExplore.Shared;
+
+public class Team
+{
+    public int Id { get; set; }
+
+    public User TeamLeader { get; set; }
+    
+    public ICollection<User> Users { get; set; }
+
+    public string TeamName { get; set; }
+
+    public Team(User teamLeader, string teamName)
+    {
+        TeamLeader = teamLeader;
+        Users = new List<User>();
+        Users.Add(teamLeader);
+        TeamName = teamName;
+    }
+}
