acae560bf8cfcba791836e1adac47509099a5159
SebastianVFugmann <90041204+SebastianVFugmann@users.noreply.github.com>
2021-11-25 12:46:49 +0100

    Ádded ConnectionType enum (#14)
    * Mooooooom, I am doooooooooone
    
    * Mom was not satisfied
    
    Co-authored-by: Sebastian Fugmann <sefu@itu.dk>

--- LitExplore/ApplicationLogic/Connection.cs
+++ LitExplore/ApplicationLogic/Connection.cs
@@ -8,15 +8,15 @@ public class Connection
 
     public Paper Paper2 { get; set; }
 
-    public string ConnectionType { get; set; }
+    public ICollection<ConnectionType> Connections { get; set; }
 
     public string? Description { get; set; }
 
-    public Connection(Paper paper1, Paper paper2, string connectionType, string? description)
+    public Connection(Paper paper1, Paper paper2, ICollection<ConnectionType> connectionTypes, string? description)
     {
         Paper1 = paper1;
         Paper2 = paper2;
-        ConnectionType = connectionType;
+        Connections = connectionTypes;
         Description = description;
     }
 }
