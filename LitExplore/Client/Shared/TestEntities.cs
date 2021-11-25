bc105482e6316d2b2b891216865c40e1ba567d60
Nadja <nako@itu.dk>
2021-11-25 14:33:45 +0100

    Inserted test entities into seperate document

---
+++ LitExplore/Client/Shared/TestEntities.cs
@@ -0,0 +1,11 @@
+namespace LitExplore.Client;
+
+public static class TestEntities {
+    public static Paper howToGetStronk = new Paper("file",new List<string>{"Caspar", "Nadja", "Theis"}, "How to get stronk", 2021,5,29, new List<string>{"training", "health"});
+    public static Paper howToMakeFood = new Paper("file1",new List<string>{"Caspar", "Theis"}, "How to make food", 2020,4,6, new List<string>{"food", "health"});
+    public static Paper hihihaha = new Paper("doc", new List<string>() { "JK ROWLING", "HAKUNA MATATA", "HJÆLP"}, "HIHIHAHA", 2012, 12, 12, null);
+
+    private static Connection testconn1 = new Connection(howToGetStronk,howToMakeFood,new List<ConnectionType>{ConnectionType.Author, ConnectionType.Tag}, null);
+    private static Connection testconn2 = new Connection(howToGetStronk,howToMakeFood,new List<ConnectionType>{ConnectionType.Author, ConnectionType.Other}, "Jeg har sat dem sammen fordi jeg godt kan lide dem.");
+    private static Connection testconn3 = new Connection(hihihaha,howToMakeFood,new List<ConnectionType>{ConnectionType.Reference, ConnectionType.Other}, "Cool papers, that I like.");
+}
