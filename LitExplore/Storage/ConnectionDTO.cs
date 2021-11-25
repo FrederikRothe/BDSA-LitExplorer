fab595fe45bfef43ccafe700da03f5508c5fc572
Frakty <66801011+FrederikRothe@users.noreply.github.com>
2021-11-25 13:50:30 +0100

    Added Team, Connection and Paper DTOs.

--- LitExplore/Storage/ConnectionDTO.cs
+++ LitExplore/Storage/ConnectionDTO.cs
@@ -1,3 +1,19 @@
 namespace Storage {
-    
+    public record ConnectionDTO(int Id, int PaperOneId, int PaperTwoId, string ConnectionType, string Description);
+
+    public record ConnectionCreateDTO
+    {
+        [Required]
+        public int PaperOneId {get; set;}
+        [Required]
+        public int PaperTwoId {get; set;}
+        [Required]
+        public string ConnectionType {get; set;}
+        public string? Description {get; set;}
+    }
+
+    public record ConnectionUpdateDTO : ConnectionCreateDTO
+    {
+        public int Id {get; set;}
+    }
 }
--- LitExplore/Storage/TeamDTO.cs
+++ LitExplore/Storage/TeamDTO.cs
@@ -1,3 +1,6 @@
-namespace Storage {
-    
-}
+namespace Storage
+
+{
+    public record TeamCreateDTO([Required] string TeamLeaderId, [Required] string TeamName);
+    public record TeamDTO(int Id, [Required] string TeamLeaderId, [Required] string TeamName) : TeamCreateDTO(TeamLeaderId, TeamName);
+} 
