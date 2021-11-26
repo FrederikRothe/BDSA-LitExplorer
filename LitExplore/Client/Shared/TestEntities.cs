eab278062b00e069b3d48f2587039bc4c63ede35
Nadja <nako@itu.dk>
2021-11-26 11:07:54 +0100

    Added testentity

--- LitExplore/Client/Shared/TestEntities.cs
+++ LitExplore/Client/Shared/TestEntities.cs
@@ -1,6 +1,7 @@
 namespace LitExplore.Client;
 
 public static class TestEntities {
+    public static Paper genericPaper = new Paper("contents",new List<string>{"An author", "another author"}, "Academic paper title", 1970,1,1, new List<string>{"academic", "scientific", "knowledge", "professionalism"});
     public static Paper howToGetStronk = new Paper("file",new List<string>{"Caspar", "Nadja", "Theis"}, "How to get stronk", 2021,5,29, new List<string>{"training", "health"});
     public static Paper howToMakeFood = new Paper("file1",new List<string>{"Caspar", "Theis"}, "How to make food", 2020,4,6, new List<string>{"food", "health"});
     public static Paper hihihaha = new Paper("doc", new List<string>() { "JK ROWLING", "HAKUNA MATATA", "HJÆLP"}, "HIHIHAHA", 2012, 12, 12, null);
