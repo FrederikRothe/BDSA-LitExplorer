00cfcaf6bca743c28b70e8b91ddaf818a01aa9a6
Nadja <nako@itu.dk>
2021-11-27 20:04:21 +0100

    Connections??

--- LitExplore/Client/GraphElements/GraphPositions.cs
+++ LitExplore/Client/GraphElements/GraphPositions.cs
@@ -2,24 +2,5 @@ namespace LitExplore.Client;
 
 public static class GraphPositions {
     public static Dictionary<Paper, (int, int)> paperPositions = new Dictionary<Paper,(int,int)>();
-    private static bool paperinitialised = false;
-
-    public static Dictionary<Paper, (int,int)> GetPaperPositions() {
-        if (!paperinitialised)
-        {
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (12,15));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (24,27));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (45,23));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (43,48));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (55,60));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (73,35));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (62,15));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (15,75));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (38,83));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (28,58));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (70,73));
-            paperPositions.Add(new Paper("yo", new List<string>(), "title", 2021,1,1,null), (85,51));
-        }
-        return paperPositions;
-    }
+    public static List<(int,int)> positions = new List<(int,int)> {(12,15),(27,24),(45,23),(43,48),(55,60),(73,35),(62,15),(15,75),(38,83),(28,58),(70,73),(85,51)};
 }
