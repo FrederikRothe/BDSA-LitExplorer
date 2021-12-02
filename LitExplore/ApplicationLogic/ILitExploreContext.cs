292bb42c282bc3d33bd3a7c238dc052e4f45481d
ehou <ehou@itu.dk>
2021-12-02 19:09:56 +0100

    Removed SaveChanges from context interface

--- LitExplore/ApplicationLogic/ILitExploreContext.cs
+++ LitExplore/ApplicationLogic/ILitExploreContext.cs
@@ -9,8 +9,6 @@ public interface ILitExploreContext : IDisposable
     DbSet<Author> Authors { get; }
     DbSet<Tag> Tags { get; }
 
-    int saveChanges();
-
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
 
