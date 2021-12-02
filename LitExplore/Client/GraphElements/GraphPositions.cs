10e7a59d72bb2f760db3eca1de2f0f0605216679
Nadja <nako@itu.dk>
2021-12-02 16:03:22 +0100

    Client fixed

--- LitExplore/Client/GraphElements/GraphPositions.cs
+++
@@ -1,6 +0,0 @@
-namespace LitExplore.Client;
-
-public static class GraphPositions {
-    public static Dictionary<Paper, (int, int)> paperPositions = new Dictionary<Paper,(int,int)>();
-    public static List<(int,int)> positions = new List<(int,int)> {(12,15),(27,24),(45,23),(43,48),(55,60),(73,35),(62,15),(15,75),(38,83),(28,58),(70,73),(85,51)};
-}
