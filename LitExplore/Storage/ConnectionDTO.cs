fd14567a7f4dff2dd0ed571f292b4cc8526dd7af
CasparMarschall <cafm@itu.dk>
2021-12-03 13:03:40 +0100

    DTOs fixed

--- LitExplore/Storage/ConnectionDTO.cs
+++ LitExplore/Storage/ConnectionDTO.cs
@@ -1,5 +1,4 @@
 namespace LitExplore.Storage;
-public record ConnectionDTO(int Id, int PaperOneId, int PaperTwoId, string? ConnectionType, string? Description);
 
 public record ConnectionCreateDTO
 {
@@ -10,7 +9,11 @@ public record ConnectionCreateDTO
     [Required]
     public string? ConnectionType { get; init; }
     public string? Description { get; init; }
+    [Required]
+    public ICollection<int> TeamIDs { get; init; }
 }
+
+public record ConnectionDTO(int Id, int PaperOneId, int PaperTwoId, string? ConnectionType, string? Description, ICollection<int> TeamIDs);
 
 public record ConnectionUpdateDTO : ConnectionCreateDTO
 {
--- LitExplore/Storage/TeamDTO.cs
+++ LitExplore/Storage/TeamDTO.cs
@@ -1,16 +1,22 @@
 namespace LitExplore.Storage;
-   
-public record TeamDTO(int Id, int TeamLeaderId, string TeamName);
 public record TeamCreateDTO
 {
-    [Required] 
+    [Required]
     public int TeamLeaderId { get; init; }
-    
+
     [Required]
     [StringLength(50)]
     public string TeamName { get; init; }
+
+    [Required]
+    public int Colour { get; init; }
+    [Required]
+    public ICollection<int> UserIDs { get; init; }
+    [Required]
+    public ICollection<int> ConnectionIDs { get; init; }
 }
-public record TeamUpdateDTO : TeamCreateDTO 
-{ 
+public record TeamDTO(int Id, int TeamLeaderId, string TeamName, int Colour, ICollection<int> UserIDs, ICollection<int> ConnectionIDs);
+public record TeamUpdateDTO : TeamCreateDTO
+{
     public int Id { get; set; }
 };
