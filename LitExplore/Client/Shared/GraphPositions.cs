4371f3d32d200eba732929de89e26815e4e37174
Nadja <nako@itu.dk>
2021-12-04 18:41:45 +0100

    More DTO implementation

--- LitExplore/Client/Shared/GraphPositions.cs
+++ LitExplore/Client/Shared/GraphPositions.cs
@@ -2,17 +2,17 @@ using System.Net.Http.Json;
 namespace LitExplore.Client;
 
 public static class GraphPositions {
-    public static Dictionary<PaperDTO, (int, int)> paperToPositions = new Dictionary<PaperDTO,(int,int)>();
+    public static Dictionary<int, (int, int)> paperToPositions { get; } = new Dictionary<int,(int,int)>();
     public static List<(int,int)> positions = new List<(int,int)> {(12,15),(27,24),(45,23),(43,48),(55,60),(73,35),(62,15),(15,75),(38,83),(28,58),(70,73),(85,51)};
     public static PaperDTO[] papers;
     private static bool initialised = false;
-    private async static void initialise(HttpClient Http) 
+    public async static void initialise(HttpClient Http) 
     {
         papers = await Http.GetFromJsonAsync<PaperDTO[]>("api/PaperManager");
 
         for (int i = 0; i<12; i++)
         {
-            paperToPositions[papers[i]] = positions[i];
+            paperToPositions[papers[i].Id] = positions[i];
         }
 
 
--- LitExplore/Client/Shared/TestEntities.cs
+++ LitExplore/Client/Shared/TestEntities.cs
@@ -1,6 +1,9 @@
 namespace LitExplore.Client;
 
 public static class TestEntities {
+    public static PaperDTO paperDTO = new PaperDTO(1,"",new List<string>(),"", 1,1,1, new List<string>());
+    public static ConnectionDTO connectionDTO = new ConnectionDTO(1,1,1,"","",new List<int>());
+    public static TeamDTO teamDTO = new TeamDTO(1,1,"",1,new List<int>(),new List<int>());
     // Authors
     public static Author nadja = new Author() { Name = "Nadja" };
     public static Author caspar = new Author() { Name = "Caspar" };
