58eb8ea4aa4a5943fee27e36d1f55b28b007cce0
Nadja <nako@itu.dk>
2021-12-04 18:14:39 +0100

    more global usings

--- LitExplore/Client/GlobalUsings.cs
+++ LitExplore/Client/GlobalUsings.cs
@@ -1,4 +1,8 @@
 global using LitExplore.ApplicationLogic;
 global using LitExplore.Client.Shared;
-global using System.Text;
 global using LitExplore.Storage;
+
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+global using System.Net.Http;
+global using System.Net.Http.Headers;
