4a81ed8ba24d1d8e114a4e8f6439ac202bdcce06
Nadja <nako@itu.dk>
2021-12-05 17:11:15 +0100

    More fixes

--- LitExplore/Client/GlobalUsings.cs
+++ LitExplore/Client/GlobalUsings.cs
@@ -4,5 +4,8 @@ global using LitExplore.Storage;
 
 global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 global using System.Net.Http;
 global using System.Net.Http.Headers;
