e166b295af6cc9fa1bdfed3ebd15dcd1934a326f
Nadja <nako@itu.dk>
2021-12-06 11:47:09 +0100

    formatting

--- LitExplore/ApplicationLogic/PaperRepository.cs
+++ LitExplore/ApplicationLogic/PaperRepository.cs
@@ -7,47 +7,27 @@ public class PaperRepository : IPaperRepository
     {
         _context = context;
     }
-    
-    // public async Task<PaperDTO> CreateAsync(PaperCreateDTO paper)
-    // {
-    //     var conflict =
-    //         await _context.Papers
-    //                         .Where(p => p.Title == paper.Title)
-    //                         .Select(p => new PaperDTO(p.Id, p.Document, FindAuthorsName(p.Authors), p.Title, p.Date.Year, p.Date.Month, p.Date.Day, p.Tags))
-    //                         .FirstOrDefaultAsync();
-
-    //     if (conflict != null)
-    //     {
-    //         return (Conflict, conflict);
-    //     }
-
-    //     var entity = new Paper
-    //     {
-    //         Authors = FindAuthorsObj((ICollection<int>)(paper.Authors)),
-    //         Title = paper.Title,
-    //         Date = new DateTime(paper.Year, paper.Month, paper.Day),
-    //         Document = paper.Document
-    //     };
-
-    //     _context.Papers.Add(entity);
-
-    //     await _context.SaveChangesAsync();
-
-    //     return new PaperDTO(entity.Id, entity.Document, FindAuthorsName(entity.Authors), entity.Title, entity.Date.Year, entity.Date.Month, entity.Date.Day, entity.Tags);
-    // }
 
     public async Task<Option<PaperDTO>> ReadAsync(int paperId)
     {
         var papers = from p in _context.Papers
-                    where p.Id == paperId
-                    select new PaperDTO(p.Id, p.Document, FindAuthorsName(p.Authors), p.Title, p.Date.Year, p.Date.Month, p.Date.Day, FindTagsName(p.Tags));
+                     where p.Id == paperId
+                     select new PaperDTO(p.Id, p.Document, FindAuthorsName(p.Authors), p.Title, p.Date.Year, p.Date.Month, p.Date.Day, FindTagsName(p.Tags));
                 
         return await papers.FirstOrDefaultAsync();
     }
 
     public async Task<IReadOnlyCollection<PaperDTO>> ReadAsync() =>
             (await _context.Papers
-                           .Select(p => new PaperDTO(p.Id, p.Document, FindAuthorsName(p.Authors), p.Title, p.Date.Year, p.Date.Month, p.Date.Day, FindTagsName(p.Tags)))
+                           .Select(p => new PaperDTO(
+                               p.Id, 
+                               p.Document, 
+                               FindAuthorsName(p.Authors), 
+                               p.Title, 
+                               p.Date.Year, 
+                               p.Date.Month, 
+                               p.Date.Day, 
+                               FindTagsName(p.Tags)))
                            .ToListAsync())
                            .AsReadOnly();
 
