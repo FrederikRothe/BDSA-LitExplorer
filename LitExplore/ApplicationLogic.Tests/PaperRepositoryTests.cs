45cc9245dbdd639f50f46ad28a212821bc86fa65
Emil Houlborg <82398520+emiloh@users.noreply.github.com>
2021-12-08 10:40:32 +0100

    Testing (#54)
    * In-Memory Datbase Seeding
    
    * Boilerplates for repositorytests
    
    Co-authored-by: ehou <ehou@itu.dk>

---
+++ LitExplore/ApplicationLogic.Tests/PaperRepositoryTests.cs
@@ -0,0 +1,43 @@
+namespace LitExplore.Infrastructure.Tests;
+
+public class PaperRepositoryTests : IDisposable 
+{
+    private readonly ILitExploreContext _context;
+    private readonly PaperRepository _repository;
+
+    private bool disposed;
+
+    public PaperRepositoryTests()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<LitExploreContext>();
+        builder.UseSqlite(connection);
+        var context = new LitExploreContext(builder.Options);
+        context.Database.EnsureCreated();
+
+        // Populate In-Memory Database
+        context = SeedInMemoryDB(context);
+
+        _context = context;
+        _repository = new PaperRepository(_context);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing) {
+                _context.Dispose();
+            }
+
+            disposed = true;
+        }    
+    }  
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
