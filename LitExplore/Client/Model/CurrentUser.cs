72dc9e02e886ea7750ba4de171435a175761f73a
Nadja <nako@itu.dk>
2021-12-10 15:49:25 +0100

    Remove hard refresh

--- LitExplore/Client/Model/CurrentUser.cs
+++ LitExplore/Client/Model/CurrentUser.cs
@@ -2,8 +2,8 @@ namespace LitExplore.Client.Model;
 
 public static class CurrentUser {
     public static string? userOid { get; set; }
-    public static TeamDTO[]? teams { get; set; }
-    public static ConnectionDTO[]? connections { get; set; }
+    public static List<TeamDTO> teams { get; set; }
+    public static List<ConnectionDTO> connections { get; set; }
     public static bool initialised() => userOid != null && teams != null && connections != null;
     public static TeamDTO? selectedTeam { get; set; }
 }
