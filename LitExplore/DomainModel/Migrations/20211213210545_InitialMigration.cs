14ef9563b1f4a06dbf8dc2d6bc4614d3af9b2895
TheisHS <74772370+TheisHS@users.noreply.github.com>
2021-12-14 08:46:25 +0100

    Scneft seeding (#98)
    * start from server
    
    * namechange
    
    * add user to team wihu
    
    * seeding fixed

--- LitExplore/DomainModel/Migrations/20211213210545_InitialMigration.cs
+++ LitExplore/DomainModel/Migrations/20211213210545_InitialMigration.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace LitExplore.DomainModel.Migrations
 {
-    public partial class seedUsers : Migration
+    public partial class InitialMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
