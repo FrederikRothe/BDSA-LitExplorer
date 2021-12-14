ab266cb00fcaeca09e9c198e0e0836bd89d92bb0
NaddiNadja <74055632+NaddiNadja@users.noreply.github.com>
2021-12-14 18:32:39 +0100

    Soft-refresh updates (#100)
    * selected team sets to 0 when clear
    
    * update graph on delete
    
    * We don't need "Model" infront
    
    * inserted null checks
    
    * shownConnections moved to model
    
    * clear when creating team
    
    * Selected team moved to Model
    
    * Auto-select selected Team
    
    * DeleteTeam refreshes graph

--- LitExplore/Client/Model/CurrentUser.cs
+++ LitExplore/Client/Model/CurrentUser.cs
@@ -4,6 +4,7 @@ public static class CurrentUser {
     public static string? userOid { get; set; }
     public static List<TeamDTO> teams { get; set; } = new List<TeamDTO>();
     public static List<ConnectionDTO> connections { get; set; } = new List<ConnectionDTO>();
+    public static List<ConnectionDTO> shownConnections { get; set; } = new List<ConnectionDTO>();
     public static bool initialised() => userOid != null && teams != null && connections != null;
     public static TeamDTO? selectedTeam { get; set; }
 
