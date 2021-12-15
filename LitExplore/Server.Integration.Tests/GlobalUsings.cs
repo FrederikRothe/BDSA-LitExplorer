b754e745462d10d1d78419500f50be58a2f9f08f
CasparMarschall <cafm@itu.dk>
2021-12-15 11:22:21 +0100

    deleted status from GU

--- LitExplore/Server.Integration.Tests/GlobalUsings.cs
+++ LitExplore/Server.Integration.Tests/GlobalUsings.cs
@@ -16,7 +16,5 @@ global using System.Security.Claims;
 global using System.Text.Encodings.Web;
 global using Xunit;
 
-
 global using LitExplore.Storage;
-global using static LitExplore.Storage.Status;
 global using LitExplore.DomainModel;
