75fdfdba4fd12ab236b0c74fd07f50094cc67c29
Frederik Rothe <66801011+FrederikRothe@users.noreply.github.com>
2021-12-16 09:42:36 +0100

    Reduced code dublication and added '_' to private fields. (#109)

--- LitExplore/Server.Integration.Tests/UserTests.cs
+++ LitExplore/Server.Integration.Tests/UserTests.cs
@@ -3,19 +3,19 @@ namespace Server.Integration.Tests;
 public class UserTests : IClassFixture<CustomWebApplicationFactory>
 {
     private readonly CustomWebApplicationFactory _factory;
+    private TestClaimsProvider _provider;
+    private HttpClient _client;
     public UserTests(CustomWebApplicationFactory factory)
     {
         _factory = factory;
+        _provider = TestClaimsProvider.WithUserClaims();
+        _client = _factory.CreateClientWithTestAuth(_provider);
     }
-
 
     [Fact]
     public async Task get_returns_users()
     {
-        var provider = TestClaimsProvider.WithUserClaims();
-        var client = _factory.CreateClientWithTestAuth(provider);
-        var user = await client.GetFromJsonAsync<UserDTO>("api/User/1");
-
+        var user = await _client.GetFromJsonAsync<UserDTO>("api/User/1");
 
         Assert.NotNull(user);
         if (user != null) 
@@ -27,10 +27,7 @@ public class UserTests : IClassFixture<CustomWebApplicationFactory>
     [Fact]
     public async Task getConnections_returns_correct_list_of_connections()
     {
-        var provider = TestClaimsProvider.WithUserClaims();
-        var client = _factory.CreateClientWithTestAuth(provider);
-        var userConnections = await client.GetFromJsonAsync<ConnectionDTO[]>("api/User/connections/1");
-
+        var userConnections = await _client.GetFromJsonAsync<ConnectionDTO[]>("api/User/connections/1");
 
         Assert.NotNull(userConnections);
         Assert.Contains(userConnections, u => u.ConnectionType == "Math");
@@ -39,9 +36,7 @@ public class UserTests : IClassFixture<CustomWebApplicationFactory>
     [Fact]
     public async Task getTeams_returns_correct_list_of_teams()
     {
-        var provider = TestClaimsProvider.WithUserClaims();
-        var client = _factory.CreateClientWithTestAuth(provider);
-        var userTeams = await client.GetFromJsonAsync<TeamDTO[]>("api/User/teams/1");
+        var userTeams = await _client.GetFromJsonAsync<TeamDTO[]>("api/User/teams/1");
 
         Assert.NotNull(userTeams);
         Assert.Contains(userTeams, u => u.TeamName == "Potato");
